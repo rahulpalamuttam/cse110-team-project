680d915c00f77e2aa5ff027c0a47d7cc6a2a581a
JeongJoon Lee <jjl112@ucsd.edu>
2015-01-28 04:48:47 -0800

    Regiteration Functionality

--- telecom/src/main/java/com/googleit/telecom/controllers/RegisterController.java
+++ telecom/src/main/java/com/googleit/telecom/controllers/RegisterController.java
@@ -1,36 +1,69 @@
 package com.googleit.telecom.controllers;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.googleit.telecom.dao.UserDAO;
 import com.googleit.telecom.models.User;
 
+/**
+ * 
+ * @author Jeongjoon Lee
+ * 
+ * Class : RegisterContorller
+ * Pakage : com.googleit.telecom.controllers
+ * 
+ * The RegisterController class manages user registeration and insert
+ * user's data where the form follows User model.
+ * 
+ */
+
 @Controller
 public class RegisterController {
-	
-	@Autowired
-	private UserDAO userDAO;
-	
-	@RequestMapping(value="/register", method = RequestMethod.GET)
-	public String registerForm(Model model) {
-		model.addAttribute("user", new User());
-		return "register/register";
-	}
-	
-	@RequestMapping(value="/register", method = RequestMethod.POST)
-	public String register(@ModelAttribute User user, Model model) {
-		// Testting...
-		java.util.Date date = new java.util.Date();
-		java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy-MM-dd");
-		String currentTime = sdf.format(date);
-		
-		user.setReg_date(currentTime);
-		userDAO.insert(user);
-		return "home";
-	}
+
+    @Autowired
+    private UserDAO userDAO;
+
+    /**
+     * Shows the registeration form
+     * 
+     * @param User
+     * @return string of regiter.html path
+     */
+    @RequestMapping("/register")
+    public String registerForm(User user) {
+        return "register/register";
+    }
+
+    /**
+     * Upon POST request register user
+     * when form validation pass.
+     * 
+     * @param User
+     * @param BindingResult
+     * @return redirect to dashboard;
+     */
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public String register(@Valid User user, BindingResult bindingResult) {
+
+        // Invalid form -> show register form page
+        if (bindingResult.hasErrors()) return "register/register";
+        
+        /* TODO :: Password Confrim validation using javascript */
+
+        // Set current time having "yyy-MM-dd" format
+        user.setReg_date(new SimpleDateFormat("yyy-MM-dd").format(new Date()));
+        userDAO.insert(user);
+
+        /* TODO :: redirect to DASHBOARD when dashboard is ready */
+        return "register/register_success";
+    }
 }
--- telecom/src/main/java/com/googleit/telecom/models/User.java
+++ telecom/src/main/java/com/googleit/telecom/models/User.java
@@ -1,52 +1,80 @@
 package com.googleit.telecom.models;
 
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 public class User {
-	private int    id;
-	private String email;
-	private String password;
-	private String last_name;
-	private String first_name;
-	private String reg_date;
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	public String getLast_name() {
-		return last_name;
-	}
-	public void setLast_name(String last_name) {
-		this.last_name = last_name;
-	}
-	
-	public String getFirst_name() {
-		return first_name;
-	}
-	public void setFirst_name(String first_name) {
-		this.first_name = first_name;
-	}
-	
-	public String getReg_date() {
-		return reg_date;
-	}
-	public void setReg_date(String reg_date) {
-		this.reg_date = reg_date;
-	}
+    
+    private int id;
+
+    @NotBlank(message = "*Please enter e-mail address")
+    @Email(message = "*Invalid e-mail form")
+    private String email;
+
+    @Size(min = 5, message="*Please enter password greater than 4")
+    private String password;
+
+    @NotBlank(message = "*Please enter last name")
+    private String last_name;
+
+    @NotBlank(message = "*Please enter first name")
+    private String first_name;
+
+    private String reg_date;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        // Hash password using BCrypt algorithm. 
+        BCryptPasswordEncoder passEncryp = new BCryptPasswordEncoder();
+        if(password.length() > 4)
+            this.password = passEncryp.encode(password);
+        else
+            this.password = password;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getReg_date() {
+        return reg_date;
+    }
+
+    public void setReg_date(String reg_date) {
+        this.reg_date = reg_date;
+    }
 }
