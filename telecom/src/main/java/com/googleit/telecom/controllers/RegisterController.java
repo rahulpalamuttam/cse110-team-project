ccf8e8647698e796c294da257b21dac6d0016369
rahulpalamuttam <rahulpalamut@gmail.com>
2015-02-01 00:46:24 -0800

    rahul did somethings

--- telecom/src/main/java/com/googleit/telecom/controllers/RegisterController.java
+++ telecom/src/main/java/com/googleit/telecom/controllers/RegisterController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.slf4j.Logger;
 
 import com.googleit.telecom.dao.UserDAO;
 import com.googleit.telecom.models.User;
@@ -31,7 +32,7 @@ public class RegisterController {
 
     @Autowired
     private UserDAO userDAO;
-
+    private static int registration = 0;
     /**
      * Shows the registeration form
      * 
@@ -56,14 +57,31 @@ public class RegisterController {
 
         // Invalid form -> show register form page
         if (bindingResult.hasErrors()) return "register/register";
-        
+
         /* TODO :: Password Confrim validation using javascript */
+
+        if(!(this.CheckPassword(user.getPassword()))){
+            System.out.println("Bad password");
+            return "/register";
+        }
 
         // Set current time having "yyy-MM-dd" format
         user.setReg_date(new SimpleDateFormat("yyy-MM-dd").format(new Date()));
         userDAO.insert(user);
-
         /* TODO :: redirect to DASHBOARD when dashboard is ready */
         return "register/register_success";
+    }
+
+    public boolean CheckPassword(String password){
+//        Logger.info("Welcome home! The client locale is {}.");
+//        if(password.length() < 10){
+//            Logger.info("Create a better password length");
+//            return false;
+//        }
+//        if(password.contains("bad")){
+//            Logger.info("create a better password");
+//            return false;
+//        }
+        return true;
     }
 }
