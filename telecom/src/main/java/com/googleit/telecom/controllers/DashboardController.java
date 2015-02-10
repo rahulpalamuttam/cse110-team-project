a0ac6071c26c29849004050552b9565bf5912328
rahulpalamuttam <rahulpalamut@gmail.com>
2015-02-10 01:25:55 -0800

    Can access the name of the authenticated user, using this snippet of code.

--- telecom/src/main/java/com/googleit/telecom/controllers/DashboardController.java
+++ telecom/src/main/java/com/googleit/telecom/controllers/DashboardController.java
@@ -1,6 +1,8 @@
 package com.googleit.telecom.controllers;
 
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,6 +12,9 @@ public class DashboardController {
 
     @RequestMapping(value="/dashboard", method = RequestMethod.GET)
     public String login() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String name = auth.getName();
+        System.out.println(name);
         return "dashboard/Customer_Dashboard";
     }
 
