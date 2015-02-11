f25df5836fd0191f6f7650d225314371bb619014
JeongJoon Lee <jjl112@ucsd.edu>
2015-02-11 00:30:00 -0800

    Fixed CustomerDAOImpl bug

--- telecom/src/main/java/com/googleit/telecom/dao/CustomerDAOImpl.java
+++ telecom/src/main/java/com/googleit/telecom/dao/CustomerDAOImpl.java
@@ -1,13 +1,20 @@
 package com.googleit.telecom.dao;
 
 import com.googleit.telecom.models.users.Customer;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+import javax.sql.DataSource;
 import java.util.List;
 
 /**
  * Created by Joon on 2/11/2015.
  */
 public class CustomerDAOImpl implements  CustomerDAO {
+    private JdbcTemplate jdbcTemplate;
+
+    public CustomerDAOImpl(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
 
     @Override
     public List<Customer> getCustomers(long customer_rep_id) {
