d014e811ab5306221ec2e5b66f5506fe91dd8600
Dean Hermanudin <dhermanu@ucsd.edu>
2015-02-19 22:47:59 -0800

    Junit test updated

--- telecom/src/test/java/com/googleit/telecom/models/items/PackageTest.java
+++ telecom/src/test/java/com/googleit/telecom/models/items/PackageTest.java
@@ -5,19 +5,29 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class PackageTest {
+	
+	Package onepackage;
 
     @Test
     public void testGetPackageID() throws Exception {
-
+       String packageID = "ATT";
+       onepackage.setPackageID(packageID);
+       assertEquals(packageID, onepackage.getPackageID());
     }
 
+    /* redundant
     @Test
     public void testSetPackageID() throws Exception {
 
-    }
+    }*/
 
     @Test
     public void testEquals() throws Exception {
-
+    	Service testerPackage = null;       
+        boolean ans = true;
+        boolean val;
+        
+        val = equals(testerPackage);
+        assertEquals(val, ans);
     }
 }
--- telecom/src/test/java/com/googleit/telecom/models/items/ServiceTest.java
+++ telecom/src/test/java/com/googleit/telecom/models/items/ServiceTest.java
@@ -5,19 +5,30 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ServiceTest {
-
+    
+	Service testService;
     @Test
     public void testGetPrice() throws Exception {
-
+       double testIDPrice = 1000;
+       testService.setPrice(testIDPrice);
+       assertEquals(testIDPrice, testService.getPrice(), 0.000);
+       
     }
 
     @Test
     public void testGetServiceID() throws Exception {
-
+    	Long testID = 12536477L;
+        testService.setServiceID(testID);
+        assertEquals(testID, testService.getServiceID());
     }
 
     @Test
     public void testEquals() throws Exception {
-
+       Service testerService = null;       
+       boolean ans = true;
+       boolean val;
+       
+       val = equals(testerService);
+       assertEquals(val, ans);
     }
 }
